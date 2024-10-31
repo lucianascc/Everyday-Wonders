a69dc1501251333611e8ba447fffd1b9919df499
lucianatux <lucianatux@yahoo.com.ar>
2024-10-31 00:55:03 -0300

    giro

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public float jumpForce = 5f;
+    public float jumpForce = 8f;
 
     private Rigidbody2D rb;
     private int jumpCount = 0; // Contador de saltos
     public int maxJumps = 2; // Número máximo de saltos permitidos
     private bool isGrounded = false; // Indica si el personaje está en el suelo
+     private bool facingRight = true; // Indica la dirección del personaje
 
     void Start()
     {
@@ -22,6 +23,16 @@ public class PlayerMovement : MonoBehaviour
         // Movimiento horizontal
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+         // Giro del personaje
+        if (moveInput > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (moveInput < 0 && facingRight)
+        {
+            Flip();
+        }
 
         // Salto
         if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || jumpCount < maxJumps))
@@ -41,5 +52,13 @@ public class PlayerMovement : MonoBehaviour
             isGrounded = true;
             jumpCount = 0; // Reinicia el contador de saltos al tocar el suelo
         }
+    }
+    // Método para voltear la dirección del personaje
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1; // Invierte la escala en el eje X
+        transform.localScale = scale;
     }
 }
